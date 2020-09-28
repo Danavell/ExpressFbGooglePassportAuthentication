1e4ff98860eb5320f743fe345d7ec4be425bb2ca
Danavell <jamesdanavell@gmail.com>
2020-09-28 13:29:13 +0100

    Implements routing for facebook auth

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const { join } = require('path')
 const express = require('express')
+const morgan = require('morgan')
 
 require('dotenv').config({ path: join(__dirname, 'config', 'config.env') })
 
-const app = express()
-
 const PORT = process.env.PORT
 const MODE = process.env.MODE
+
+if (MODE === 'development') {
+    app.use(morgan('dev'))
+}
+
+const app = express()
 
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT} in ${MODE} mode`)
