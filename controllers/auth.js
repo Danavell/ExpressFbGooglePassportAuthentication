935e47edaa1c33744bad33d7e12b7feb9f94f9c3
Danavell <jamesdanavell@gmail.com>
2020-09-28 13:31:16 +0100

    Implements facebook strategy for OAuth authentication

---
+++ controllers/auth.js
@@ -0,0 +1,33 @@
+const passport = require("passport")
+const strategy = require("passport-facebook")
+
+const FacebookStrategy = strategy.Strategy
+
+passport.serializeUser(function (user, done) {
+    done(null, user)
+})
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+})
+
+passport.use(
+    new FacebookStrategy(
+        {
+            clientID: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+            callbackURL: process.env.FACEBOOK_CALLBACK_URL,
+            profileFields: ['id', 'email', 'name']
+        },
+        function (accessToken, refreshToken, profile, done) {
+            const { email, first_name, last_name } = profile._json;
+            const userData = {
+                email,
+                firstName: first_name,
+                lastName: last_name
+            };
+            console.log(userData)
+            done(null, profile);
+        }
+    )
+)
